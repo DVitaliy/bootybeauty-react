ecb1fd12cf143120b365f0e1854ae70412647215
Vitaliy <dynnik.vitaliy@gmail.com>
2020-06-18 00:40:27 +0300

    modal

---
+++ src/components/modal.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import '../styles/Modal.css'
+
+//https://tympanus.net/Development/ModalWindowEffects/
+const Modal = () => {
+  return <div>modal</div>
+}
+export default Modal
